9c829263c4807cc9bb047b59d0a15e7ca5e58cff
Karlla Souzza <karlla.souzza7@gmail.com>
2026-03-13 03:59:11 -0300

    feat(user): implement user model with authentication
    - Create User model extending UserSchema
    - Integrate authentication capabilities using withAuthFinder
    - Add methods for role typing and token abilities

---
+++ src/infrastructure/persistence/lucid/models/user.ts
@@ -0,0 +1,19 @@
+import { UserSchema } from '#database/schema'
+import hash from '@adonisjs/core/services/hash'
+import { compose } from '@adonisjs/core/helpers'
+import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import { type AccessToken, DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
+import type { Role } from '../../../../domain/users/role/role.ts'
+
+export default class User extends compose(UserSchema, withAuthFinder(hash)) {
+  static accessTokens = DbAccessTokensProvider.forModel(User)
+  declare currentAccessToken?: AccessToken
+
+  get typedRole(): Role {
+    return this.role as Role
+  }
+
+  getTokenAbilities(): string[] {
+    return this.currentAccessToken?.abilities || []
+  }
+}
