e2cf4c3063034ce49772172d0a3b88e377b6e235
Karlla Souzza <karlla.souzza7@gmail.com>
2026-03-13 23:55:31 -0300

    feat(routes): update user signup and retrieval routes
    - Changed signup route to use Self controller
    - Updated user retrieval routes to use correct controller methods

--- start/routes.ts
+++ start/routes.ts
@@ -6,7 +6,7 @@ router
   .group(() => {
     router
       .group(() => {
-        router.post('signup', [controllers.Users, 'store'])
+        router.post('signup', [controllers.Self, 'store'])
         router.post('login', [controllers.AccessToken, 'store'])
         router.delete('logout', [controllers.AccessToken, 'destroy']).use(middleware.auth())
       })
@@ -15,7 +15,10 @@ router
 
     router
       .group(() => {
-        router.get('/', [controllers.Users, 'showAll'])
+        router.get('/', [controllers.users.GetManyUsers, 'handler'])
+        router.post('/', [controllers.users.CreateUser, 'handler'])
+        router.put('/:id', [controllers.users.UpdateUserById, 'handler'])
+        router.delete('/:id', [controllers.users.DeleteUserById, 'handler'])
       })
       .prefix('users')
       .as('users')
